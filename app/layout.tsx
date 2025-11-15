e51eb7e99b3172cc5fcef961d92647da511feec7
Vin-dictive <vinay.valson@gmail.com>
2025-11-15 14:35:17 -0800

    Added ico for my website

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Vinay Valson - Developer Profile',
   description: 'Software Engineer & Data Scientist Portfolio',
+  icons: {
+    icon: '/favicon.svg',
+  },
 }
 
 export default function RootLayout({
